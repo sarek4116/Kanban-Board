dd62e57de24c103db25c6ce3cba0f2fd187ba6cb
Aleksander Sar <sarek4116@gmail.com>
2019-06-19 13:53:44 +0200

    fix some bugs

--- js/script.js
+++ js/script.js
@@ -24,7 +24,8 @@ document.addEventListener('DOMContentLoaded', function () {
         this.id = randomString();
         this.name = name;
         this.element = generateTemplate('column-template', {
-            name: this.name
+            name: this.name,
+            id: this.id
         });
         this.element.querySelector('.column').addEventListener('click', function (event) {
             if (event.target.classList.contains('btn-delete-column')) {
